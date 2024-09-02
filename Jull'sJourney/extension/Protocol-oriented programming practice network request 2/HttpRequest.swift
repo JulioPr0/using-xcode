aaa459c5bbd4f2d83e59dc99bbe225d7b06309ba
julpramaitama <julio.pramaitama@binus.ac.id>
2024-09-03 00:04:17 +0700

    protocol oriented programming practice network request 2

--- Jull'sJourney/extension/Protocol-oriented programming practice network request 2/HttpRequest.swift
+++ Jull'sJourney/extension/Protocol-oriented programming practice network request 2/HttpRequest.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum HttpRequestMethod: String
+{
+    case GET
+    case POST
+    case PUT
+    case DELETE
+}
+
+protocol HttpRequest
+{
+    var path: String { get }
+    var method: HttpRequestMethod { get }
+    var parameter: [String: Any] { get }
+    
+    associatedtype Response: DecodableEntity
+}
+
+extension HttpRequest
+{
+    var host : String { return "https://websitejulio.com" }
+}
