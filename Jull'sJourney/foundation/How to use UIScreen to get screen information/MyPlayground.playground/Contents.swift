7799b38b627948c843f66d9c1a097a5fffd57ee4
julpramaitama <julio.pramaitama@binus.ac.id>
2024-09-19 23:38:01 +0700

    ui screen to get screen information added

---
+++ Jull'sJourney/foundation/How to use UIScreen to get screen information/MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+UIScreen.main.availableModes.description
+UIScreen.screens.count
+UIScreen.main.bounds
+UIScreen.main.nativeBounds
+UIScreen.main.nativeScale
+UIScreen.main.brightness
+UIScreen.main.coordinateSpace.bounds
+UIScreen.main.currentMode?.size
