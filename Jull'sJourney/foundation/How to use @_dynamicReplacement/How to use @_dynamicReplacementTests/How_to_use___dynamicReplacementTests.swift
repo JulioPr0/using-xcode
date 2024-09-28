3efa31934d7d9555e35b45646ed76b74723c8cfc
julpramaitama <julio.pramaitama@binus.ac.id>
2024-09-28 12:05:27 +0700

    thurs until sat updated

---
+++ Jull'sJourney/foundation/How to use @_dynamicReplacement/How to use @_dynamicReplacementTests/How_to_use___dynamicReplacementTests.swift
@@ -0,0 +1,12 @@
+//___FILEHEADER___
+
+import Testing
+@testable import ___VARIABLE_productName:identifier___
+
+struct ___FILEBASENAME:identifier___ {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
