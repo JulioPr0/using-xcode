351a12cd6cc0f1a636827943fdfd83246bdb9d8b
julpramaitama <julio.pramaitama@binus.ac.id>
2024-11-27 16:00:08 +0700

    selection quick inserttion added

---
+++ Jull'sJourney/advanced/The Quick sorting algorithm/The Quick sorting algorithmTests/The_Quick_sorting_algorithmTests.swift
@@ -0,0 +1,17 @@
+//
+//  The_Quick_sorting_algorithmTests.swift
+//  The Quick sorting algorithmTests
+//
+//  Created by Julio Pramaitama on 27/11/24.
+//
+
+import Testing
+@testable import The_Quick_sorting_algorithm
+
+struct The_Quick_sorting_algorithmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
