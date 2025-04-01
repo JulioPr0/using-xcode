373922424d111947c9c23a54a7b403f0743c7f4a
julpramaitama <julio.pramaitama@binus.ac.id>
2025-04-01 23:04:07 +0700

    interactive objective-c basics added

---
+++ Objective-C/Basic syntax of Objective-C language/DemoProject/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  DemoProject
+//
+//  Created by Julio Pramaitama on 01/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, Objective-C! \n");
+        //Helping text in programe
+        int startCount;
+        int _startCount;
+        int _startCount8;
+//        int 8startCount;
+//        int start*Count;
+//        int union;
+        int StartCount;
+        
+//        intappleCount;
+        int appleCount;
+        appleCount = 4 + 6;
+        appleCount=4+6;
+    }
+    return 0;
+}
